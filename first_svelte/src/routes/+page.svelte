b1ace9e4f8a438631ace18499bcdd0310e8c210b
gilmarneto <jumas.neto@gmail.com>
2025-05-30 11:17:48 -0300

    Contador

--- first_svelte/src/routes/+page.svelte
+++ first_svelte/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- JS -->
+<script>
+    // Variáveis
+    let numero = 0
+    // Função decremento
+    function decremento() {
+        numero -= 1
+    }
+    // Função reset
+    function reset() {
+        numero = 0
+    }
+    // Função incremento
+    function incremento() {
+        numero += 1
+    }
+</script>
+
+<!-- HTML -->
+<h1>Contador</h1>
+<div><h2>{numero}</h2></div>
+<button on:click={decremento}>-1</button>
+<button on:click={reset}>reset</button>
+<button on:click={incremento}>+1</button>
--- first_svelte/vite.config.js
+++ first_svelte/vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: { 
-		port: 8083 // <--- Defina a porta desejada aqui 
+		port: 8083, // <--- Defina a porta desejada aqui
+		host: true,
+		allowedHosts: ['homegneto.ddns.net'] // <--- Para uso via NO-IP
 	} 
 });
